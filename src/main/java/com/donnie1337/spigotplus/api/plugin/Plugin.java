0f7eda48172279a85f018c6a6fc5fccb16de0f51
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-13 23:27:59 -0300

    revert: remove unnecessary plugin API base

--- src/main/java/com/donnie1337/spigotplus/api/plugin/Plugin.java
+++
@@ -1,8 +0,0 @@
-package com.donnie1337.spigotplus.api.plugin;
-
-/** Minimal stable lifecycle contract exposed to SpigotPlus plugins. */
-public interface Plugin {
-    default void onLoad() {}
-    default void onEnable() {}
-    default void onDisable() {}
-}
